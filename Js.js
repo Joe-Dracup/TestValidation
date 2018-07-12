b68fc60cf69c1f32f1109facad303a41b3e8c8cf
Joe Dracup <32868247+Joe-Dracup@users.noreply.github.com>
2018-07-12 10:06:10 +0100

    Added validation against spaces

--- Js.js
+++ Js.js
@@ -1,4 +1,25 @@
- $('#inpt').on('input', function () {
-        console.log('Input Detected')
-    });
+$('#inpt').on('input', function () {
+    var input = GetInput('inpt')
+    console.log(input)
+    var space = ContainsSpace(input)
+    console.log(space)
+    if(space){
+        document.getElementById('validation').innerHTML = 'Please do not enter a space';
+    }
+    else{
+        document.getElementById('validation').innerHTML = '';
+    }
+});
 
+function GetInput(id){
+    return document.getElementById(id).value;
+}
+
+function ContainsSpace(input){
+    var space = " ";
+    if(input.includes(space)){
+       return true;
+       }
+       else{
+       return false;
+       }}
